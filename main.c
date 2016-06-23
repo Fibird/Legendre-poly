8f5b11dac1d8cdf59e0a22dd03a3f7c65bc26db9
Chaoyang Liu <chaoyanglius@gmail.com>
2016-06-23 22:44:42 +0800

    fix a error that '2n' should be '2n+1'

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ double legendre(int n, double x)
     }
     else
     {
-        return ((2 * n) * x * legendre(n - 1, x) - (n - 1) * legendre(n-2, x)) / n;
+        return ((2 * n - 1) * x * legendre(n - 1, x) - (n - 1) * legendre(n-2, x)) / n;
     }
 }
 
